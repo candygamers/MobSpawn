508db4f1c6f2967b75ee3be1206575d6022d6e98
candygamers <candygamers1232@gmail.com>
2024-10-06 00:47:34 +0300

    Initial commit

---
+++ src/main/java/candygamers/github/io/spawnMob/Main.java
@@ -0,0 +1,13 @@
+package candygamers.github.io.spawnMob;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
